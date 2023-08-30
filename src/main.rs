6a4fb3b132d80bf2eab0153149765eac25d747ae
Scott Cowe <scott.t.cowe@gmail.com>
2023-08-30 21:13:09 +0100

    Implemented the Sieve of Eratosthenes for generating prime numbers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
 fn main() {
-    println!("Hello, world!");
+    let primes: Vec<u32> = find_primes_smaller_than(100);
+
+    for prime in primes {
+        println!("{}", prime)
+    }
+}
+
+// Uses Sieve of Eratosthenes algorithm
+fn find_primes_smaller_than(n: u32) -> Vec<u32> {
+    let mut bool_results: Vec<bool> = vec![true; n as usize + 1];
+    let root_n: u32 = (n as f32).sqrt() as u32;
+
+    let mut i: u32 = 2;
+
+    while i <= root_n {
+        let mut j: u32 = i * i;
+
+        while j <= n {
+            bool_results[j as usize] = false;
+            j += i;
+        }
+
+        i += 1;
+    }
+
+    let mut results: Vec<u32> = Vec::new();
+    let mut result_index: u32 = 0;
+
+    for result in bool_results {
+        if result == true && result_index > 1 {
+            results.push(result_index);
+        }
+
+        result_index += 1;
+    }
+
+    results
 }
